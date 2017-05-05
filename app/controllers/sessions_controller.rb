eaf7e8cfcf6821e7ca321493f16225f603a10a67
wencakisa <wencakisa@gmail.com>
2017-05-05 21:22:58 +0300

    Fixed little bug in log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = 'You have been successfully logged out.'
     redirect_to root_path
   end
