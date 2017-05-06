4a3285fa48f721985c3420dee49672f9386fab49
wencakisa <wencakisa@gmail.com>
2017-05-06 21:03:36 +0300

    Little UI improvements

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,10 @@
 module UsersHelper
-  def gravatar_for(user, size: 350)
+  def gravatar_for(user, size: 350, centered: false)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar img-circle")
+    img_class = 'img-circle img-responsive'
+    img_class << ' img-centered' if centered
+
+    image_tag(gravatar_url, alt: user.name, class: img_class)
   end
 end
